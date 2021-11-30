1946d19cab432ae862e093e254eea894557124d7
kisublee <yigi2021@gmail.com>
2021-11-30 18:21:03 -0500

    Add Link to Nav and Add Route to App.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import {Route, Routes, Navigate} from "react-router-dom";
 /*
   Components
 */
@@ -33,9 +33,14 @@ class App extends React.Component {
     return (
       <div className="wrapper">
         <Nav />
-        <Home employees={employees} owners={owners} pets={pets} />
-        <StaffList employees={employees} />
-        <PetsList pets={pets} />
+      
+        <Routes>
+        <Route exact path="/" element={ <Home employees={employees} owners={owners} pets={pets} />} />
+        <Route path="/staff" element={ <StaffList employees={employees} />} />
+        <Route path="/pets/*" element={   <PetsList pets={pets} />} />
+        </Routes>
+       
+     
         <Footer />
       </div>
     );
--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,6 +1,8 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import {Route, Routes} from "react-router-dom";
+
 
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
@@ -14,18 +16,24 @@ export const PetsList = ({ pets }) => {
 
   return (
     <section className="pets-wrapper">
+      <Routes>
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
         {/* All cats section */}
-        {cats.map((cat) => (
+        <Route path="/pets/cats" render={cats.map((cat) => (
           <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+        ))} />
+
+        {/* {cats.map((cat) => (
+          <Pet key={cat.id} kind="cat" pet={cat} />
+        ))} */}
 
         {/* All dogs section */}
         {dogs.map((dog) => (
           <Pet key={dog.id} kind="dog" pet={dog} />
         ))}
       </section>
+      </Routes>
     </section>
   );
 };
