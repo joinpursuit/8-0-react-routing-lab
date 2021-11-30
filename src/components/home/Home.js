dcd25df8443581ba0610df3d49ad6a8c6721760a
JoshuaEwuoso <joshuaewuoso@pursuit.org>
2021-11-30 13:39:51 -0500

    import data info

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -2,8 +2,11 @@ import Highlights from "./Highlights";
 import NewsletterForm from "./NewsletterForm";
 import splashImage from "./andrew-s-unsplash.jpg";
 import "./Home.css";
+import { employees } from "../../data/employees.js";
+import { owners } from "../../data/owners";
+import { pets } from "../../data/pets";
 
-export const Home = ({ employees, owners, pets }) => {
+export const Home = () => {
   return (
     <main>
       <section className="splash-image">
