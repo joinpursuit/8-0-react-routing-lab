f58bc2c54d7319b99d7610f06c72acf3b9b16a5c
Triane Peart <TrianePeart@gmail.com>
2021-11-30 17:22:56 -0500

    Completed

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ import { employees } from "./data/employees.js";
 import { owners } from "./data/owners";
 import { pets } from "./data/pets";
 
+import {Link, Switch, Route} from "react-router-dom"
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -33,9 +35,17 @@ class App extends React.Component {
     return (
       <div className="wrapper">
         <Nav />
-        <Home employees={employees} owners={owners} pets={pets} />
-        <StaffList employees={employees} />
-        <PetsList pets={pets} />
+        <Switch>
+        <Route exact path="/">
+          <Home employees={employees} owners={owners} pets={pets} />
+        </Route>
+        <Route path="/staff">
+          <StaffList employees={employees} />
+        </Route>
+          <Route path="/pets">
+          <PetsList pets={pets} />
+        </Route>
+        </Switch>
         <Footer />
       </div>
     );
--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,6 +1,7 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import {Route, Switch, Redirect} from "react-router-dom";
 
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
@@ -13,21 +14,22 @@ export const PetsList = ({ pets }) => {
   );
 
   return (
-    <section className="pets-wrapper">
-      <PetsListNav cats={cats} dogs={dogs} />
-      <section className="pets-list">
-        {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
-
-        {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+  <section className="pets-wrapper">
+    <PetsListNav cats={cats} dogs={dogs} />
+    <section className="pets-list">
+      <Switch>
+          <Route exact path="/pets">
+            <Redirect to="/pets/cats" />
+          </Route>
+          <Route path="/pets/cats">
+          {cats.map((cat) => (<Pet key={cat.id} kind="cat" pet={cat} />))}
+          </Route>
+          <Route path="/pets/dogs">
+          {dogs.map((dog) => (<Pet key={dog.id} kind="dog" pet={dog} />))}
+          </Route>
+      </Switch>
       </section>
     </section>
   );
 };
-
 export default PetsList;
