38fca410abef247f33f5414d950ed6cac14a94a9
Angie G <angieg@Angies-MBP.fios-router.home>
2021-12-01 12:17:11 -0500

    All Files

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,6 +1,7 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { Route, Switch, Redirect } from "react-router";
 
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
@@ -16,15 +17,23 @@ export const PetsList = ({ pets }) => {
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
-        {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
-
-        {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        <Switch>
+          <Route exact path="/pets">
+            <Redirect to="/pets/cats" />
+          </Route>
+          <Route path="/pets/cats">
+            {/* All cats section */}
+            {cats.map((cat) => (
+              <Pet key={cat.id} kind="cat" pet={cat} />
+            ))}
+          </Route>
+          <Route path="/pets/dogs">
+            {/* All dogs section */}
+            {dogs.map((dog) => (
+              <Pet key={dog.id} kind="dog" pet={dog} />
+            ))}
+          </Route>
+        </Switch>
       </section>
     </section>
   );
