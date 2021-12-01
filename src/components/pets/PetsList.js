c0678899d9ad2c648c5c7722c16c92aa2459ca41
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2021-12-01 10:47:38 -0500

    All Components Render

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,44 +1,36 @@
 import PetsListNav from "./PetsListNav";
-import Pet from "./Pet";
 import "./PetsList.css";
-import { render } from "@testing-library/react";
-import { Component } from "react";
+import { Redirect, Route } from "react-router-dom";
 import pets from "../../data/pets";
+import Cats from "./Cats";
+import Dogs from "./Dogs";
 
-class PetsList extends Component {
-  constructor() {
-    super();
-    this.state = {
-      pets,
-    };
-  }
-  render() {
-    const [cats, dogs] = pets.reduce(
-      (acc, pet) => {
-        const position = pet.kind === "Cat" ? 0 : 1;
-        acc[position].push(pet);
-        return acc;
-      },
-      [[], []]
-    );
+const PetsList = () => {
+  const [cats, dogs] = pets.reduce(
+    (acc, pet) => {
+      const position = pet.kind === "Cat" ? 0 : 1;
+      acc[position].push(pet);
+      return acc;
+    },
+    [[], []]
+  );
 
-    return (
-      <section className="pets-wrapper">
-        <PetsListNav cats={cats} dogs={dogs} />
-        <section className="pets-list">
-          {/* All cats section */}
-          {cats.map((cat) => (
-            <Pet key={cat.id} kind="cat" pet={cat} />
-          ))}
+  return (
+    <section className="pets-wrapper">
+      <PetsListNav cats={cats} dogs={dogs} />
 
-          {/* All dogs section */}
-          {dogs.map((dog) => (
-            <Pet key={dog.id} kind="dog" pet={dog} />
-          ))}
-        </section>
+      <section className="pets-list">
+        {/* All cats section */}
+        <Route path="/pets/cats" component={Cats} />
+        {/* All dogs section */}
+        <Route path="/pets/dogs" component={Dogs} />
+        {/* Quickest way to render components */}
+        {/* <Route path="/pets/cats"> {**JSX Element w. props / Component alone / Mapped elements /render any of those } </Route> */}
+        //
+        <Redirect to="/pets/cats"> {Cats} </Redirect>
       </section>
-    );
-  }
-}
+    </section>
+  );
+};
 
 export default PetsList;
