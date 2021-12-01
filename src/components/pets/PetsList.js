60d2b4a1f8255205f5c4df6055d8e5b9a6f9b32b
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2021-11-30 22:07:03 -0500

    Added Links and PetList Component Rendering

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,33 +1,44 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { render } from "@testing-library/react";
+import { Component } from "react";
+import pets from "../../data/pets";
 
-export const PetsList = ({ pets }) => {
-  const [cats, dogs] = pets.reduce(
-    (acc, pet) => {
-      const position = pet.kind === "Cat" ? 0 : 1;
-      acc[position].push(pet);
-      return acc;
-    },
-    [[], []]
-  );
+class PetsList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      pets,
+    };
+  }
+  render() {
+    const [cats, dogs] = pets.reduce(
+      (acc, pet) => {
+        const position = pet.kind === "Cat" ? 0 : 1;
+        acc[position].push(pet);
+        return acc;
+      },
+      [[], []]
+    );
 
-  return (
-    <section className="pets-wrapper">
-      <PetsListNav cats={cats} dogs={dogs} />
-      <section className="pets-list">
-        {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+    return (
+      <section className="pets-wrapper">
+        <PetsListNav cats={cats} dogs={dogs} />
+        <section className="pets-list">
+          {/* All cats section */}
+          {cats.map((cat) => (
+            <Pet key={cat.id} kind="cat" pet={cat} />
+          ))}
 
-        {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+          {/* All dogs section */}
+          {dogs.map((dog) => (
+            <Pet key={dog.id} kind="dog" pet={dog} />
+          ))}
+        </section>
       </section>
-    </section>
-  );
-};
+    );
+  }
+}
 
 export default PetsList;
