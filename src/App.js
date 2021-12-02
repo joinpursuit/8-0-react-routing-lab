558ca0a4749c5c6a4cc3922f8ebf91857b45de4a
Eva Rodriguez <evarodriguez@Evas-MBP.lan>
2021-12-01 19:09:47 -0500

    Finished Lab

--- src/App.js
+++ src/App.js
@@ -1,45 +1,19 @@
-import React from "react";
-
-/*
-  Components
-*/
-import Nav from "./components/common/Nav";
 import Footer from "./components/common/Footer";
-import Home from "./components/home/Home";
-import StaffList from "./components/staff/StaffList";
-import PetsList from "./components/pets/PetsList";
+import Nav from "./components/common/Nav";
+import Main from "./components/Main";
 
-/*
-  Data
-  ---------------
-  Note: Normally this data would be pulled from an API. It is not necessary, however, for this application.
-*/
-import { employees } from "./data/employees.js";
-import { owners } from "./data/owners";
-import { pets } from "./data/pets";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      employees,
-      owners,
-      pets,
-    };
-  }
+export const App = () => {
 
-  render() {
-    const { employees, owners, pets } = this.state;
     return (
-      <div className="wrapper">
-        <Nav />
-        <Home employees={employees} owners={owners} pets={pets} />
-        <StaffList employees={employees} />
-        <PetsList pets={pets} />
-        <Footer />
-      </div>
+      <>
+        <div className="wrapper">
+          <Nav/>
+                <Main />
+          <Footer/>
+        </div>
+      </>
     );
-  }
 }
 
 export default App;
--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -2,7 +2,8 @@ import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
 
-export const PetsList = ({ pets }) => {
+
+export const PetsList = ({ pets, isCat }) => {
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
@@ -12,20 +13,20 @@ export const PetsList = ({ pets }) => {
     [[], []]
   );
 
+  const showCats =cats.map((cat) => (
+    <Pet key={cat.id} kind="cat" pet={cat} />
+  ))
+  const showDogs =dogs.map((dog) => (
+    <Pet key={dog.id} kind="dog" pet={dog} />
+  ))
+
   return (
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
-        {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
-
-        {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        {isCat ? showCats : showDogs}
       </section>
+
     </section>
   );
 };
