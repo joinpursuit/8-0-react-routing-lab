f0acb616311a4ebd2548f9af34a14ae69e16886d
shareekaepps <shareekaepps@icloud.com>
2021-12-14 23:03:49 -0500

    petList added

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,9 +1,10 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 export const PetsList = ({ pets }) => {
-  const [cats, dogs] = pets.reduce(
+  const [cats, dogs] = pets.reduce (
     (acc, pet) => {
       const position = pet.kind === "Cat" ? 0 : 1;
       acc[position].push(pet);
@@ -17,17 +18,25 @@ export const PetsList = ({ pets }) => {
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
         {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
 
+        <Switch>
+          <Route exact path="/pets" >
+            <Redirect to="/pets/cats" />
+          </Route>
+          <Route path="/pets/cats" >
+            { cats.map((cat) => (
+            <Pet key={cat.id} kind="cat" pet={cat} />
+          ))}
+          </Route>
         {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+          <Route path="/pets/dogs" >
+              { dogs.map((dog) => (
+              <Pet key={dog.id} kind="dog" pet={dog} />
+              ))}
+          </Route> 
+        </Switch>
+        </section>
       </section>
-    </section>
-  );
-};
+  )};
 
 export default PetsList;
