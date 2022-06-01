04626e9de5feed81825526a3f7b64f6ea92a454b
EmaleeSoto <emaleesoto@gmail.com>
2022-06-01 17:55:32 -0400

    Added routes and links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 /*
   Components
@@ -32,10 +33,22 @@ class App extends React.Component {
     const { employees, owners, pets } = this.state;
     return (
       <div className="wrapper">
-        <Nav />
-        <Home employees={employees} owners={owners} pets={pets} />
-        <StaffList employees={employees} />
-        <PetsList pets={pets} />
+        <Router>
+          <Nav />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Home employees={employees} owners={owners} pets={pets} />
+              }
+            />
+            <Route
+              path="/staff"
+              element={<StaffList employees={employees} />}
+            />
+            <Route path="/pets//*" element={<PetsList pets={pets} />} />
+          </Routes>
+        </Router>
         <Footer />
       </div>
     );
