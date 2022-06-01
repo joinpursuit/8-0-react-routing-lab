e4597b4348c719c85cb73ac2c656256768425c2c
Lortiz528 <lortiztech@gmail.com>
2022-06-01 17:12:39 -0400

    passed index tests

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 /*
   Components
 */
-import Nav from "./components/common/Nav";
-import Footer from "./components/common/Footer";
-import Home from "./components/home/Home";
-import StaffList from "./components/staff/StaffList";
-import PetsList from "./components/pets/PetsList";
+import Nav from './components/common/Nav';
+import Footer from './components/common/Footer';
+import Home from './components/home/Home';
+import StaffList from './components/staff/StaffList';
+import PetsList from './components/pets/PetsList';
 
 /*
   Data
   ---------------
   Note: Normally this data would be pulled from an API. It is not necessary, however, for this application.
 */
-import { employees } from "./data/employees.js";
-import { owners } from "./data/owners";
-import { pets } from "./data/pets";
+import { employees } from './data/employees.js';
+import { owners } from './data/owners';
+import { pets } from './data/pets';
 
 class App extends React.Component {
   constructor(props) {
@@ -32,14 +33,32 @@ class App extends React.Component {
     const { employees, owners, pets } = this.state;
     return (
       <div className="wrapper">
-        <Nav />
-        <Home employees={employees} owners={owners} pets={pets} />
-        <StaffList employees={employees} />
-        <PetsList pets={pets} />
-        <Footer />
+        <Router>
+          <Nav />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Home employees={employees} owners={owners} pets={pets} />
+              }
+            />
+            <Route
+              path="/staff"
+              element={<StaffList employees={employees} />}
+            />
+            <Route path="/pets" element={<PetsList pets={pets} />} />
+          </Routes>
+          <Footer />
+        </Router>
       </div>
     );
   }
+}
+
+{
+  /* <Home employees={employees} owners={owners} pets={pets} />
+            <StaffList employees={employees} />
+            <PetsList pets={pets} /> */
 }
 
 export default App;
