5cd2bee84d32cf0083884bcf43268d363ccce815
H.C.Guerrero <clydeguerrero1@gmail.com>
2022-06-02 00:33:12 -0300

    All but one nav and one staff  cypress tests pass

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React from 'react';
 
 /*
   Components
 */
-import Nav from "./components/common/Nav";
-import Footer from "./components/common/Footer";
-import Home from "./components/home/Home";
-import StaffList from "./components/staff/StaffList";
-import PetsList from "./components/pets/PetsList";
+import Nav from './components/common/Nav';
+import Footer from './components/common/Footer';
+import Home from './components/home/Home';
+import StaffList from './components/staff/StaffList';
+import PetsList from './components/pets/PetsList';
 
 /*
   Data
   ---------------
   Note: Normally this data would be pulled from an API. It is not necessary, however, for this application.
 */
-import { employees } from "./data/employees.js";
-import { owners } from "./data/owners";
-import { pets } from "./data/pets";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { employees } from './data/employees.js';
+import { owners } from './data/owners';
+import { pets } from './data/pets';
 
 class App extends React.Component {
   constructor(props) {
@@ -31,12 +32,31 @@ class App extends React.Component {
   render() {
     const { employees, owners, pets } = this.state;
     return (
-      <div className="wrapper">
-        <Nav />
-        <Home employees={employees} owners={owners} pets={pets} />
-        <StaffList employees={employees} />
-        <PetsList pets={pets} />
-        <Footer />
+      <div className='wrapper'>
+        <Router>
+          <Nav />
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <Home employees={employees} owners={owners} pets={pets} />
+              }
+            />
+            <Route path='/employees'>
+              <Route
+                index
+                element={<StaffList employees={employees} type={'Employees'} />}
+              />
+              <Route path=':id' element={<StaffList employees={employees} />} />
+            </Route>
+
+            <Route path='/pets'>
+              <Route index element={<PetsList pets={pets} type={'Pets'} />} />
+              <Route path=':id' element={<PetsList pets={pets} />} />
+            </Route>
+          </Routes>
+          <Footer />
+        </Router>
       </div>
     );
   }
