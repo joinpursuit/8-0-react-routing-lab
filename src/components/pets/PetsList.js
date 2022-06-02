1d5271cbd1a2daf4ff7b3b788ed97f6ececbb852
JC-MT <jancarlosmatias@gmail.com>
2022-06-02 09:24:49 -0400

    practice

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,63 +1,62 @@
-import PetsListNav from "./PetsListNav";
-import Pet from "./Pet";
-import "./PetsList.css";
-import { Route, Routes } from "react-router-dom";
+import PetsListNav from './PetsListNav';
+import Pet from './Pet';
+import './PetsList.css';
+import { Route, Routes } from 'react-router-dom';
 
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
-      const position = pet.kind === "Cat" ? 0 : 1;
+      const position = pet.kind === 'Cat' ? 0 : 1;
       acc[position].push(pet);
       return acc;
     },
     [[], []]
   );
 
+  {
+    /* All cats section */
+  }
+  const allCats = cats.map((cat) => <Pet key={cat.id} kind="cat" pet={cat} />);
+
+  const allDogs = dogs.map((dog) => <Pet key={dog.id} kind="dog" pet={dog} />);
+
   return (
-      <section className="pets-wrapper">
-        <PetsListNav cats={cats} dogs={dogs} />
-        <section className="pets-list">
-          <Routes>
-            <Route 
-              path='/'
-              element={
+    <section className="pets-wrapper">
+      <PetsListNav cats={cats} dogs={dogs} />
+      <section className="pets-list">
+        <Routes>
+          <Route
+            path="/"
+            element={
               <div>
-              {/* All cats section */}
-              {cats.map((cat) => (
-                <Pet key={cat.id} kind="cat" pet={cat} />
-              ))}
-              {/* All dogs section */}
-              {dogs.map((dog) => (
-                  <Pet key={dog.id} kind="dog" pet={dog} />
-                ))}
-              </div>
-              }
-            />
-            <Route
-              path='/cats'
-              element={
-                <div>
                 {/* All cats section */}
-                {cats.map((cat) => (
-                  <Pet key={cat.id} kind="cat" pet={cat} />
-                ))}
-                </div>
-                }
-            />
-            <Route
-              path='/dogs'
-              element={
-                <div>
-              {/* All dogs section */}
-              {dogs.map((dog) => (
-                  <Pet key={dog.id} kind="dog" pet={dog} />
-                ))}
-                </div>
-                }
-            />
-          </Routes>
-        </section>
+                {allCats}
+                {/* All dogs section */}
+                {allDogs}
+              </div>
+            }
+          />
+          <Route
+            path="/cats"
+            element={
+              <div>
+                {/* All cats section */}
+                {allCats}
+              </div>
+            }
+          />
+          <Route
+            path="/dogs"
+            element={
+              <div>
+                {/* All dogs section */}
+                {allDogs}
+              </div>
+            }
+          />
+        </Routes>
       </section>
+    </section>
   );
 };
 
