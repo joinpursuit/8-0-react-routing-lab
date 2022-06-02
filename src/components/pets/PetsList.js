b97ca4131dee5ad170b55cb8a53dfbba2c1ccb31
Matt M <Afrodesiacc@gmail.com>
2022-06-01 21:17:09 -0400

    Completed,

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,6 +1,7 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { Routes, Route } from "react-router-dom";
 
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
@@ -12,19 +13,24 @@ export const PetsList = ({ pets }) => {
     [[], []]
   );
 
+  const catList = cats.map((cat) => (
+    <Pet key={cat.id} kind="cat" pet={cat} />
+  ))
+  const dogList = dogs.map((dog) => (
+    <Pet key={dog.id} kind="dog" pet={dog} />
+  ))
+  const allPetsList = <div>{catList} {dogList}</div>
+
   return (
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
-        {/* All cats section */}
-        {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
-
-        {/* All dogs section */}
-        {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        <Routes>
+          <Route path="/" element={allPetsList}/>
+          <Route path="cats" element={catList} />
+          <Route path="dogs" element={dogList} />
+        </Routes>
+        {/* {allPetsList} */}
       </section>
     </section>
   );
