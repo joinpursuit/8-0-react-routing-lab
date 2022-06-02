a8e3a070b56b13f56cd392143d3299af2b9b0821
named.josie <named.josie@gmail.com>
2022-06-01 21:02:11 -0400

    updated petlist file

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,34 +1,38 @@
-import PetsListNav from "./PetsListNav";
-import Pet from "./Pet";
-import "./PetsList.css";
-import { Routes, Route } from "react-router-dom";
+import PetsListNav from './PetsListNav';
+import Pet from './Pet';
+import './PetsList.css';
+import { Routes, Route } from 'react-router-dom';
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
-      const position = pet.kind === "Cat" ? 0 : 1;
+      const position = pet.kind === 'Cat' ? 0 : 1;
       acc[position].push(pet);
       return acc;
     },
-    [[], []]
+    [[], []],
   );
 
   return (
-    <section className="pets-wrapper">
+    <section className='pets-wrapper'>
       <PetsListNav cats={cats} dogs={dogs} />
-      <section className="pets-list">
-       
+      <section className='pets-list'>
         <Routes>
-           {/* All cats section */}
-       <Route path='/cats' element= {cats.map((cat) => (
-          <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}/>
+          {/* All cats section */}
+          <Route
+            path='/cats'
+            element={cats.map((cat) => (
+              <Pet key={cat.id} kind='cat' pet={cat} />
+            ))}
+          />
 
-        {/* All dogs section */}
-        <Route path='/cats' element= {dogs.map((dog) => (
-          <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}/>
-
-      </Routes>
+          {/* All dogs section */}
+          <Route
+            path='/cats'
+            element={dogs.map((dog) => (
+              <Pet key={dog.id} kind='dog' pet={dog} />
+            ))}
+          />
+        </Routes>
       </section>
     </section>
   );
