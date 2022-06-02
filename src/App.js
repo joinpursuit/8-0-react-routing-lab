49954fbef0c9ba927d9fab302eb27cd45452769e
Cesar Ortiz <ceazthemoment@aol.com>
2022-06-02 09:59:47 -0400

    lab route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 /*
   Components
@@ -32,11 +33,17 @@ class App extends React.Component {
     const { employees, owners, pets } = this.state;
     return (
       <div className="wrapper">
+        <Router>
         <Nav />
-        <Home employees={employees} owners={owners} pets={pets} />
-        <StaffList employees={employees} />
-        <PetsList pets={pets} />
-        <Footer />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home employees={employees} owners={owners} pets={pets}/>}/>
+              <Route path="staff" element={<StaffList employees={employees}/>} />
+              <Route path="pets/*" element={<PetsList pets={pets}/>} />
+            </Routes>
+          </main>
+          </Router>
+          <Footer />
       </div>
     );
   }
