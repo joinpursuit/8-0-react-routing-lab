c92337af59d1b2c86f2508e26a38a38e7fb3636d
named.josie <named.josie@gmail.com>
2022-06-01 21:01:13 -0400

    updated petlist file

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,7 +1,7 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
-
+import { Routes, Route } from "react-router-dom";
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
     (acc, pet) => {
@@ -16,15 +16,19 @@ export const PetsList = ({ pets }) => {
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
-        {/* All cats section */}
-        {cats.map((cat) => (
+       
+        <Routes>
+           {/* All cats section */}
+       <Route path='/cats' element= {cats.map((cat) => (
           <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+        ))}/>
 
         {/* All dogs section */}
-        {dogs.map((dog) => (
+        <Route path='/cats' element= {dogs.map((dog) => (
           <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        ))}/>
+
+      </Routes>
       </section>
     </section>
   );
