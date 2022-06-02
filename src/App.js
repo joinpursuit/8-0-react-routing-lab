da16502a94e6de22cb8d2c382b6bdfc9af22d686
named.josie <named.josie@gmail.com>
2022-06-01 20:48:56 -0400

    updated app and nav files

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import PetsList from "./components/pets/PetsList";
 import { employees } from "./data/employees.js";
 import { owners } from "./data/owners";
 import { pets } from "./data/pets";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 class App extends React.Component {
   constructor(props) {
@@ -32,11 +33,15 @@ class App extends React.Component {
     const { employees, owners, pets } = this.state;
     return (
       <div className="wrapper">
+        <Router>
         <Nav />
-        <Home employees={employees} owners={owners} pets={pets} />
-        <StaffList employees={employees} />
-        <PetsList pets={pets} />
+        <Routes>
+        <Route path="/" element={<Home employees={employees} owners={owners} pets={pets}/>}  />
+        <Route path="/staff" element= {<StaffList employees={employees}/> } />
+        <Route path="/pet/*" elemente={<PetsList pets={pets}/>} />
+        </Routes>
         <Footer />
+        </Router>
       </div>
     );
   }
