8ad61ec70f579fce2e5be8989e587d23e03bd2d4
Michael Kleemoff <michaelkleemoff2@gmail.com>
2022-06-05 12:11:11 -0400

    First and final commit.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,68 @@
-import React from "react";
+/** @format */
+
+import React from 'react';
 
 /*
   Components
 */
-import Nav from "./components/common/Nav";
-import Footer from "./components/common/Footer";
-import Home from "./components/home/Home";
-import StaffList from "./components/staff/StaffList";
-import PetsList from "./components/pets/PetsList";
+import Nav from './components/common/Nav';
+import Footer from './components/common/Footer';
+import Home from './components/home/Home';
+import StaffList from './components/staff/StaffList';
+import PetsList from './components/pets/PetsList';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 /*
   Data
   ---------------
   Note: Normally this data would be pulled from an API. It is not necessary, however, for this application.
 */
-import { employees } from "./data/employees.js";
-import { owners } from "./data/owners";
-import { pets } from "./data/pets";
+import { employees } from './data/employees.js';
+import { owners } from './data/owners';
+import { pets } from './data/pets';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      employees,
-      owners,
-      pets,
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			employees,
+			owners,
+			pets,
+		};
+	}
 
-  render() {
-    const { employees, owners, pets } = this.state;
-    return (
-      <div className="wrapper">
-        <Nav />
-        <Home employees={employees} owners={owners} pets={pets} />
-        <StaffList employees={employees} />
-        <PetsList pets={pets} />
-        <Footer />
-      </div>
-    );
-  }
+	render() {
+		const { employees, owners, pets } = this.state;
+		return (
+			<div className='wrapper'>
+				<Router>
+					<Nav />
+					<Routes>
+						<Route
+							path='/'
+							element={
+								<Home employees={employees} owners={owners} pets={pets} />
+							}
+						/>
+						<Route
+							path='/staff'
+							element={<StaffList employees={employees} />}
+						/>
+						<Route path='/pets' element={<PetsList pets={pets} />} />
+						<Route
+							path='/pets/:cats'
+							element={<PetsList pets={pets} kind='Cat' />}
+						/>
+						<Route
+							path='/pets/:dogs'
+							element={<PetsList pets={pets} kind='Dog' />}
+						/>
+					</Routes>
+					<Footer />
+				</Router>
+			</div>
+		);
+	}
 }
 
 export default App;
