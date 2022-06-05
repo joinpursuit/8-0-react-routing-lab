5f2d322e1e3d86c7d78bd1b3f6380e84e9df759e
Samantha Figueroa <samanthafigueroa@pursuit.org>
2022-06-05 12:34:04 -0400

    lab done

--- src/components/pets/PetsList.js
+++ src/components/pets/PetsList.js
@@ -1,6 +1,7 @@
 import PetsListNav from "./PetsListNav";
 import Pet from "./Pet";
 import "./PetsList.css";
+import { useParams, Link } from "react-router-dom";
 
 export const PetsList = ({ pets }) => {
   const [cats, dogs] = pets.reduce(
@@ -12,19 +13,20 @@ export const PetsList = ({ pets }) => {
     [[], []]
   );
 
+  const {id} = useParams();
+
   return (
     <section className="pets-wrapper">
       <PetsListNav cats={cats} dogs={dogs} />
       <section className="pets-list">
-        {/* All cats section */}
-        {cats.map((cat) => (
+        {id === "cats" ? cats.map((cat) => (
           <Pet key={cat.id} kind="cat" pet={cat} />
-        ))}
+        )) : null}
 
-        {/* All dogs section */}
-        {dogs.map((dog) => (
+        {id === 'dogs' ? dogs.map((dog) => (
           <Pet key={dog.id} kind="dog" pet={dog} />
-        ))}
+        )) : null}
+        
       </section>
     </section>
   );
--- src/components/pets/PetsListNav.js
+++ src/components/pets/PetsListNav.js
@@ -1,14 +1,21 @@
 import "./PetsListNav.css";
+import { Link } from "react-router-dom";
 
 export const PetsListNav = ({ cats, dogs }) => {
   return (
     <nav className="pets-nav">
       <ul>
         <li>
-          <a href="/pets/cats">See All Cats ({cats.length})</a>
+          <Link to="/pets/cats">
+          See All Cats ({cats.length})
+          </Link>
+          {/* <a href="/pets/cats">See All Cats ({cats.length})</a> */}
         </li>
         <li>
-          <a href="/pets/dogs">See All Dogs ({dogs.length})</a>
+          <Link to="/pets/dogs">
+          See All Dogs ({dogs.length})
+          </Link>
+          {/* <a href="/pets/dogs">See All Dogs ({dogs.length})</a> */}
         </li>
       </ul>
     </nav>
