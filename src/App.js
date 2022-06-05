61563ab91f1d03f9ddfe3f66639430b93c884355
Christine Taylor <ctay47@icloud.com>
2022-06-05 12:52:02 -0400

    completed

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import PetsList from "./components/pets/PetsList";
 import { employees } from "./data/employees.js";
 import { owners } from "./data/owners";
 import { pets } from "./data/pets";
+import {BrowserRouter as Router,Routes, Route} from "react-router-dom"
 
 class App extends React.Component {
   constructor(props) {
@@ -30,13 +31,37 @@ class App extends React.Component {
 
   render() {
     const { employees, owners, pets } = this.state;
+    
+    const cats = pets.filter((cat) => {
+      return cat.kind == "Cat"
+    })
+    const dogs = pets.filter((dog) => {
+    return dog.kind == "Dog"
+  })
     return (
       <div className="wrapper">
-        <Nav />
-        <Home employees={employees} owners={owners} pets={pets} />
-        <StaffList employees={employees} />
-        <PetsList pets={pets} />
-        <Footer />
+        <Router>
+          <Nav />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Home employees={employees} owners={owners} pets={pets} />
+              }
+            />
+            <Route
+              path="/staff"
+              element={<StaffList employees={employees} />}
+            />
+            <Route path="/pets" element={<PetsList pets={pets} />} />
+            <Route path="/pets/cats" element={<PetsList pets={cats} />} />
+            <Route path="/pets/dogs" element={<PetsList pets={dogs} />} />
+            
+            
+            
+          </Routes>
+            <Footer />
+        </Router>
       </div>
     );
   }
--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,9 +1,20 @@
 import "./Footer.css";
+import {Link} from "react-router-dom"
 
 export const Footer = () => {
   return (
     <footer>
       <p>Northwest Animal Hospital. 2022.</p>
+      <ul>
+        <li>
+          <Link to="/staff">All Pets
+          </Link>
+        </li>
+        <li>
+          <Link to="/pets">All Staff
+          </Link>
+        </li>
+      </ul>
     </footer>
   );
 };
